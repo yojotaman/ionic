bc7a647fcb30ca2a1ac410ea98aaafa87cc6ebfb
yojotaman <yojotaman@gmail.com>
2017-04-05 09:17:48 -0500

    se crea UI para notificaciones y perfil, se añade información de prueba para visualización

--- ionic_tter/src/pages/about/about.ts
+++ ionic_tter/src/pages/about/about.ts
@@ -8,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+   notifications:Array<Notifications> = [
+      {action: 'reply', img:'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario1', text:'Me encanta esta aplicación'},
+      {action: 'replatzitt', img:'https://images.pexels.com/photos/101584/pexels-photo-101584.jpeg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario2', text:'Excelente poder tenerla en mi celular'},
+      {action: 'mention', img:'https://images.pexels.com/photos/206559/pexels-photo-206559.jpeg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario3', text:'cualquier cosa'}
+    ];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface Notifications{
+  img:string;
+  nick_name:string;
+  text:string;
+  action:string;
 }
--- ionic_tter/src/pages/contact/contact.ts
+++ ionic_tter/src/pages/contact/contact.ts
@@ -8,8 +8,64 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+   notifications:Array<Notifications> = [
+      {action: 'reply', img:'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario1', text:'Me encanta esta aplicación'},
+      {action: 'replatzitt', img:'https://images.pexels.com/photos/101584/pexels-photo-101584.jpeg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario2', text:'Excelente poder tenerla en mi celular'},
+      {action: 'mention', img:'https://images.pexels.com/photos/206559/pexels-photo-206559.jpeg?w=940&h=650&auto=compress&cs=tinysrgb', nick_name:'@usuario3', text:'cualquier cosa'}
+    ];
 
+  me:Me = {
+    "img":'https://images.pexels.com/photos/206559/pexels-photo-206559.jpeg?w=940&h=650&auto=compress&cs=tinysrgb',
+    "nick_name":'@usuario3',
+    "interactions":1,
+    "github": "usuario3",
+    "twitter": "usuario3",
+    "medium": "usuario3",
+    "bio": "usuario3 es uno de nuestros amigos más listos, realiza acrobacias y le gustan los deportes extremos."
+  };
+
+
+  listado:Array<twitt> = [
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@usuario3', text:'test 1234'}
+    ];
+
+    
+
+
+  constructor(public navCtrl: NavController) {
+    this.me.interactions = this.listado.length;
   }
 
+
+}
+
+interface Me{
+  img:string;
+  nick_name:string;
+  interactions:number;
+  github:string;
+  twitter:string;
+  medium:string;
+  bio:string;
+}
+
+
+interface Notifications{
+  img:string;
+  nick_name:string;
+  text:string;
+  action:string;
+}
+
+interface twitt{
+  img:string;
+  nick_name:string;
+  text:string;
 }
